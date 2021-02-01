d2f6b1f3dbf0f95047419f7d87a1caa077070ebd
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-01 15:30:49 +0530

    feat(example): add working example

--- example/server.go
+++ example/server.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"fmt"
+	gin_middleware "github.com/UniAuth/gin-middleware"
+	"github.com/UniAuth/gin-middleware/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
+	// initialize gin
 	app := gin.New()
 	app.Use(gin.Logger())
+
+	// initialize multiple UniAuth Application Configurations
+	var configs = []models.ApplicationConfig{
+		models.ApplicationConfig{
+			Name:            "server1",
+			Url:             "http://localhost:5000",
+			ClientId:        "600ee6ec924dd75267384cb4",
+			ClientSecret:    "986727d0-c253-4adb-a9b8-c233a89cdb25",
+			RedirectUri:     "http://localhost:3000/callback",
+			AuthEndpoint:    "account/o/login",
+			ProfileEndpoint: "account/o/access",
+			ProfileProcessor: func(details string) {
+				fmt.Println(details)
+			},
+		},
+
+		models.ApplicationConfig{
+			Name:            "server2",
+			Url:             "http://localhost:4000",
+			ClientId:        "600ee6ec924dd714752a8f74",
+			ClientSecret:    "986727d0-147a-258a-3a6a-q7s4f5s8z3a6",
+			RedirectUri:     "http://localhost:3000/callback",
+			AuthEndpoint:    "account/o/login",
+			ProfileEndpoint: "account/o/access",
+			ProfileProcessor: func(details string) {
+				fmt.Println(details)
+			},
+		},
+	}
+
+	// initialize uniAuth middleware instances
+	uniAuth := gin_middleware.Init(configs)
+
+	// landing page
 	app.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"alive": true})
 	})
+
+	// url to initiate login
+	app.GET("/login", uniAuth.Authenticate("server1"))
 
 	app.Run(":3000")
 }
--- uniauth.go
+++ uniauth.go
@@ -2,35 +2,47 @@ package gin_middleware
 
 import (
 	"fmt"
+	"github.com/UniAuth/gin-middleware/minion"
 	"github.com/UniAuth/gin-middleware/models"
 	"github.com/gin-gonic/gin"
 )
 
 var uniAuthInstance models.UniAuthInstance
+var appConfigs []models.ApplicationConfig
 
-func saveConfigurations(c models.ConfigStruct) {
-	fmt.Print(c)
-}
-
+// AuthenticateMiddleware redirects user to authorization page
 func AuthenticateMiddleware(configName string) gin.HandlerFunc {
+	// return instance of gin middleware
 	return func(c *gin.Context) {
-		//	mojo for redirecting user to auth endpoint
+		// check if configName is valid
+		config, err := minion.GetConfigByName(appConfigs, configName)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Config named %s was not found, using %s", configName, config.Name))
+		}
+
+		// generate url of UniAuth Server
+		authLink := fmt.Sprintf("%s/%s?client_id=%s&redirect_uri=%s", config.Url, config.AuthEndpoint, config.ClientId, config.RedirectUri)
+
+		// take a permanent redirect to auth server
+		c.Redirect(302, authLink)
 	}
 }
 
+// CallBackMiddleware handles the response from OAuth server and fetches user details
 func CallBackMiddleware(configName string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//	mojo for receiving callback from user
 	}
 }
 
-func Init(config models.ConfigStruct) models.UniAuthInstance {
+// Init acts as a constructor to initiate the auth module
+func Init(configs []models.ApplicationConfig) models.UniAuthInstance {
 	uniAuthInstance = models.UniAuthInstance{
 		Callback:     CallBackMiddleware,
 		Authenticate: AuthenticateMiddleware,
 	}
 	fmt.Println("UniAuth Module Injected")
-	saveConfigurations(config)
+	appConfigs = configs
 
 	return uniAuthInstance
 }
