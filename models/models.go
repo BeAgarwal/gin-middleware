b9e14a7691593d78297b1a79a0aec6a723643a98
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-01 15:30:34 +0530

    feat(auth): working redirection middleware

--- models/models.go
+++ models/models.go
@@ -2,21 +2,20 @@ package models
 
 import "github.com/gin-gonic/gin"
 
-// ProfileProcessor to process incoming user profile from server
-type ProfileProcessor func(userDetails string)
-
-type ConfigStruct struct {
-	Name            string
-	Url             string
-	ClientId        string
-	ClientSecret    string
-	RedirectUri     string
-	AuthEndpoint    string
-	ProfileEndpoint string
-}
-
+type ProfileProcessorStruct func(userDetails string)
 type AuthenticateStruct func(configName string) gin.HandlerFunc
 type CallbackStruct func(configName string) gin.HandlerFunc
+
+type ApplicationConfig struct {
+	Name             string
+	Url              string
+	ClientId         string
+	ClientSecret     string
+	RedirectUri      string
+	AuthEndpoint     string
+	ProfileEndpoint  string
+	ProfileProcessor ProfileProcessorStruct
+}
 
 type UniAuthInstance struct {
 	Authenticate AuthenticateStruct
